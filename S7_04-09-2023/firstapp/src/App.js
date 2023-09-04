67bcc889c164676388adc376fde89f5331f17901
Kabir <sagithedeveloper@gmail.com>
2023-09-04 15:39:42 +0530

    Added the code

---
+++ S7_04-09-2023/firstapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header.js";
+import Main from "./Main";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to React JS</h2>
+
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ S7_04-09-2023/firstapp/src/Header.js
@@ -0,0 +1,25 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+      <h2>Header Component</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+        tempore saepe atque nisi asperiores, facere nihil vitae porro nam
+        distinctio non ea expedita doloremque est aspernatur eos quaerat enim.
+        Provident, non, ratione unde debitis voluptas consectetur molestiae
+        quisquam quasi modi doloribus incidunt, delectus ea est! Optio magni
+        corporis libero aliquid. Aliquam quo earum voluptas molestiae laboriosam
+        soluta totam placeat, vero saepe accusamus quibusdam. Nesciunt vitae
+        dolorum id explicabo sapiente maxime nemo dolorem aliquam cumque, unde
+        expedita et doloribus labore dolor non vero nulla? Repellendus, quod eum
+        provident molestiae error quae rem eveniet tempora numquam, temporibus
+        iusto dolore, nemo architecto dignissimos?
+      </p>
+      <button>Profile Details</button>
+    </div>
+  );
+}
+
+export default Header;
